349ae140fc7496c0c66aab4d39f4948e4d5b10fd
izasoerya <ihza.surya@gmail.com>
2023-11-09 22:24:48 +0700

    feat : finish run task

--- include/Scheduler.h
+++ include/Scheduler.h
@@ -2,7 +2,6 @@
 #define SCHEDULELR_H
 
 #include <Arduino.h>
-#include <vector.h>
 
 class TaskScheduler{
 private:
@@ -11,15 +10,16 @@ private:
         String name;
         uint16_t delay;
         uint8_t idTask;
+        uint32_t prevMillis = 0;
+        void *theTask;
     };
     createTaskParam taskData[24]; 
-    uint32_t prevMillis;
     uint8_t iterationTask = 0;
 
 public:
     TaskScheduler();
-    void createTask(uint8_t idTask, String name, uint16_t delay);
-    void runTask(createTaskParam taskData);
+    void createTask(uint8_t idTask, String name, uint16_t delay, void (*thisF)());
+    void runTask(uint8_t idTask);
     void suspendTask(uint8_t idTask);
     void resumeTask(uint8_t idTask);
 };
--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -2,20 +2,21 @@
 
 TaskScheduler::TaskScheduler(){}
 
-void TaskScheduler::createTask(uint8_t idTask, String name, uint16_t delay)
+void TaskScheduler::createTask(uint8_t idTask, String name, uint16_t delay, void (*theTask)())
 {
-    taskData[iterationTask].idTask = idTask;
-    taskData[iterationTask].name = name;
-    taskData[iterationTask].delay = delay;
+    taskData[idTask].idTask = idTask;
+    taskData[idTask].name = name;
+    taskData[idTask].delay = delay;
+    taskData[idTask].theTask = theTask;
     iterationTask++;
 }
 
-void TaskScheduler::runTask(createTaskParam taskData)
+void TaskScheduler::runTask(uint8_t idTask)
 {
-    if(millis() - prevMillis > taskData.delay)
+    if(millis() - taskData[idTask].prevMillis > taskData[idTask].delay)
     {
-        prevMillis = millis();
-        Serial.println(taskData.name);
-    }
-        
+        void (*theTask)() = reinterpret_cast<void (*)(void)>(taskData[idTask].theTask);
+        theTask();
+        taskData[idTask].prevMillis = millis();
+    }  
 }
