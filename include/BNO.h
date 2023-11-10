a522fd9bb4d8ff20b1c06928c7bfcd07ff5f5165
rhnathar <zildan.athar@gmail.com>
2023-11-10 15:38:33 +0700

    feat: added new function

--- include/BNO.h
+++ include/BNO.h
@@ -3,19 +3,32 @@
 
 #include <Adafruit_BNO055.h>
 
-class BNO {
+class BNO{
   public:
     BNO();
     void begin();
+    void calibrate();
     void readSensor();
+    void printResult();
     float getHeading();
     float getRoll();
     float getPitch();
+    float getAccelerationX();
+    float getAccelerationY();
+    float getAccelerationZ();
+    float getTemperature();
+    
   private:
     Adafruit_BNO055 bno;
     float heading;
     float roll;
     float pitch;
+    float headingOffset;
+    float rollOffset;
+    float pitchOffset;
+    float accelerationX;
+    float accelerationY;
+    float accelerationZ;
 };
 
 #endif
--- src/BNO.cpp
+++ src/BNO.cpp
@@ -1,26 +1,78 @@
-
 #include "BNO.h"
 
-BNO::BNO() {
-  // Constructor
-}
+BNO::BNO() {}
 
 void BNO::begin() {
   // Initialize the BNO sensor
+  if(!bno.begin()) {
+    Serial.print("No BNO055 detected ... Check your wiring!");
+    while(1);
+  }
+}
+
+void BNO::calibrate() {
+  // Calibrate the BNO sensor
+  imu::Vector<3> euler;
+  //wait for 2 second till the sensor is stable
+  for(int i = 0; i < 30; i++) {
+    euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    delay(100);
+  }
+  headingOffset = euler.x();
+  rollOffset = euler.y();
+  pitchOffset = euler.z();
 }
 
 void BNO::readSensor() {
-  // Read data from the BNO sensor
-}
+  // Get new data from the BNO sensor
+  //Get the orientation angles
+  imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  heading = euler.x() - headingOffset;
+  roll = euler.y() - rollOffset;
+  pitch = euler.z() - pitchOffset;
 
+  if(heading < 0) {
+    heading = 360 + heading;
+  }
+
+  if(roll < 0) {
+    roll = 360 + roll;
+  }
+
+  if(pitch < 0) {
+    pitch = 360 + pitch;
+  }
+
+  //get the acceleration data
+  imu::Vector<3> acceleration = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+  accelerationX = acceleration.x();
+  accelerationY = acceleration.y();
+  accelerationZ = acceleration.z();
+}
+void BNO::printResult() {
+  // Print the result
+  Serial.print("Heading: ");
+  Serial.print(heading);
+  Serial.print(" Roll: ");
+  Serial.print(roll);
+  Serial.print(" Pitch: ");
+  Serial.println(pitch);
+}
 float BNO::getHeading() {
-  // Get the heading from the BNO sensor
+  return heading;
 }
-
-float BNO::getPitch() {
-  // Get the pitch from the BNO sensor
-}
-
 float BNO::getRoll() {
-  // Get the roll from the BNO sensor
+  return roll;
+}
+float BNO::getPitch() {
+  return pitch;
+}
+float BNO::getAccelerationX() {
+  return accelerationX;
+}
+float BNO::getAccelerationY() {
+  return accelerationY;
+}
+float BNO::getAccelerationZ() {
+  return accelerationZ;
 }
