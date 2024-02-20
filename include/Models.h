d2aebb84fc07a08b606e242a5148caf0253d1675
izasoerya <ihza.surya@gmail.com>
2024-02-20 20:26:29 +0700

    feat: add datatype for lora message structure

--- include/Models.h
+++ include/Models.h
@@ -1,10 +1,20 @@
 #ifndef MODELS_H
 #define MODELS_H
 
-enum rocketState{
+enum rocketState
+{
     initialization = 1,
     flying = 2,
     landing = 3,
+};
+
+class DataType
+{
+    float angleX;
+    float angleY;
+    float angleZ;
+    float altitude;
+    rocketState state;
 };
 
 #endif
