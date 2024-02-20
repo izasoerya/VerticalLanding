f133b1e9001cc6aa0ecc8efe6a69987f71ec4cf2
rhnathar <zildan.athar@gmail.com>
2024-02-20 21:07:33 +0700

    feat: ms5611 lib

---
+++ include/ms5611Sensor.h
@@ -0,0 +1,16 @@
+#ifndef ms5611Sensor_H
+#define ms5611Sensor_H
+
+#include "MS5611.h"
+
+class ms5611Sensor{
+    public:
+        ms5611Sensor();
+        void begin();
+        float readPressure();
+        float getAltitude(double pressure, double basePressure);
+    private:
+    MS5611 ms5611;
+};
+
+#endif
