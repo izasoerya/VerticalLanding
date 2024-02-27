cfef5e843fe7ebbb2337d621b58676de3be981a3
rhnathar <zildan.athar@gmail.com>
2024-02-27 19:41:49 +0700

    update ms5611 header

--- include/ms5611Sensor.h
+++ include/ms5611Sensor.h
@@ -7,7 +7,8 @@ class ms5611Sensor{
     public:
         ms5611Sensor();
         void begin();
-        float readPressure();
+        void read();
+        float getPressure();
         float getAltitude(double pressure, double basePressure);
     private:
     MS5611 ms5611;
--- src/ms5611Sensor.cpp
+++ src/ms5611Sensor.cpp
@@ -1,22 +1,31 @@
 #include "ms5611Sensor.h"
 
 ms5611Sensor::ms5611Sensor(){
-    ms5611 = MS5611();
+    ms5611 = MS5611(0x77);
 }
 
 void ms5611Sensor::begin(){
-    while(!ms5611.begin())
+    if (ms5611.begin() == true)
     {
-        Serial.print("No MS5611 detected ...");
-        delay(500);
+        Serial.println("MS5611 found.");
+        ms5611.setOversampling(OSR_ULTRA_HIGH);
     }
-    ms5611.setOversampling(OSR_ULTRA_HIGH);
+    else
+    {
+        Serial.println("MS5611 not found. halt.");
+        while (1);
+    }
+    Serial.println();
 }
 
-float ms5611Sensor::readPressure(){
+void ms5611Sensor::read(){
+    ms5611.read();
+}
+
+float ms5611Sensor::getPressure(){
     return ms5611.getPressure();
 }
 
 float ms5611Sensor::getAltitude(double pressure, double basePressure){
-    return (44330.0f * (1.0f - pow((double)pressure / (double)basePressure, 0.1902949f)));
+    return ((273.15f + ms5611.getTemperature()) / 0.0065f * (-1.0f + pow((double)basePressure / (double)pressure, 0.19022256039f)));
 }
